053720963f3151ed06ec9ebd9fa6a0eb10c6b193
Jason Dentler <jasondentler@gmail.com>
2011-06-29 20:35:44 -0500

    Fricken BUGS in nCQRS.

--- src/Restbucks.Domain.Tests/NcqrsModule.cs
+++ src/Restbucks.Domain.Tests/NcqrsModule.cs
@@ -2,6 +2,7 @@
 using Ncqrs.Commanding.ServiceModel;
 using Ncqrs.Domain;
 using Ncqrs.Eventing.ServiceModel.Bus;
+using Ncqrs.Eventing.Storage;
 using Ncqrs.Saga;
 using Ncqrs.Saga.Mapping;
 using Ninject;
@@ -21,33 +22,50 @@ namespace Restbucks
             Kernel.Bind<IUnitOfWorkFactory>()
                 .To<UnitOfWorkFactory>();
 
-            var commandService = new CommandService();
-            commandService.Configure();
-
-            var queuedCommandService = new QueuedCommandService(commandService);
-
-            Kernel.Bind<ICommandService>()
-                .ToConstant(queuedCommandService);
-
             Kernel.Bind<IUniqueIdentifierGenerator>()
                 .ToConstant(new BasicGuidGenerator());
 
+            Kernel.Bind<ISagaCreationStrategy>()
+                .To<SagaCreationStrategy>();
+
+            SetupEventStorage();
+            SetupCommandService();
+            SetupEventBus();
+
+        }
+
+        private void SetupEventStorage()
+        {
+            const string connectionStringName = "EventStore";
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[connectionStringName];
+            var store = new Ncqrs.Eventing.Storage.SQL.MsSqlServerEventStore(connectionString.ConnectionString);
+            Kernel.Bind<IEventStore>()
+                .ToConstant(store);
+        }
+
+        private void SetupCommandService()
+        {
+            var commandService = new CommandService();
+            commandService.Configure();
+            var queuedCommandService = new QueuedCommandService(commandService);
+            Kernel.Bind<ICommandService>()
+                .ToConstant(queuedCommandService);
+        }
+
+        private void SetupEventBus()
+        {
             var eventBus = new InProcessEventBus();
-            var asm = typeof (Restbucks.Billing.MenuItemAddedHandler).Assembly;
+            var asm = typeof(Restbucks.Billing.MenuItemAddedHandler).Assembly;
             eventBus.RegisterAllHandlersInAssembly(asm,
                                                    t => Kernel.Get(t));
-
-            eventBus.RegisterSagaMappingsIn(typeof (DeliverySagaMapping).Assembly);
+            eventBus.RegisterSagaMappingsIn(typeof(DeliverySagaMapping).Assembly);
 
             var aggregateRootCreatedHandler = new EntityCreatedHandler();
             aggregateRootCreatedHandler.RegisterWith(eventBus);
 
             Kernel.Bind<IEventBus>()
                 .ToConstant(eventBus);
-
-            Kernel.Bind<ISagaCreationStrategy>()
-                .To<SagaCreationStrategy>();
-
         }
+
     }
 }
