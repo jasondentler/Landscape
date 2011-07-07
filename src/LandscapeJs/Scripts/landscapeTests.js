8b638a7f9b9640fd4774df04d6cf9eb45671ccd7
Jason Dentler <jasondentler@gmail.com>
2011-07-07 06:31:35 -0500

    Better example project

---
+++ src/LandscapeJs/Scripts/landscapeTests.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="jquery-1.6.2.js"/>
+/// <reference path="jQuery.tmpl.js"/>
+/// <reference path="knockout-1.2.1.debug.js"/>
+/// <reference path="jquery-ui-1.8.14.js"/>
+/// <reference path="landscape.js"/>
+
+module('viewModel function');
+
+test('Undefined model returns empty view model', function () {
+    window.model = undefined;
+    var actual = landscape.viewModel();
+    deepEqual(actual, {});
+})
+
+test('Defined model returns model as view model', function () {
+    var expected = { name: 'Landscape' };
+    window.model = expected;
+    var actual = landscape.viewModel();
+    equal(actual, expected);
+});
+
+module('templateFailedToLoad function');
+
+test('Displays a modal UI dialog', function() {
+    landscape.templateFailedToLoad();
+});
