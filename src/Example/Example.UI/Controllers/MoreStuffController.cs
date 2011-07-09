5f836bd59d2c42ae35ee60ff6e3ad1b484216732
Jason Dentler <jasondentler@gmail.com>
2011-07-09 13:16:38 -0500

    Added other stuff, more stuff, and careers controllers and actions, added Careers content

---
+++ src/Example/Example.UI/Controllers/MoreStuffController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Example.UI.Controllers
+{
+    public class MoreStuffController : Controller
+    {
+        //
+        // GET: /OtherStuff/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/Example/Example.UI/Controllers/OtherStuffController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Example.UI.Controllers
+{
+    public class OtherStuffController : Controller
+    {
+        //
+        // GET: /OtherStuff/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+    }
+}
